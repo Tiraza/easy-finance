1a4d47388ff86ee29e63fe38c0ce02ee4ca20611
muryllo.santos <m.tiraza@gmail.com>
2015-04-30 17:28:37 -0300

    Navigation Drawer

--- app/src/main/java/extractor/com/myapplication/ui/EasyFinanceActivity.java
+++ app/src/main/java/extractor/com/myapplication/ui/EasyFinanceActivity.java
@@ -3,21 +3,20 @@ package extractor.com.myapplication.ui;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Toast;
 
 import com.mikepenz.materialdrawer.Drawer;
 import com.mikepenz.materialdrawer.model.DividerDrawerItem;
 import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
 import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
 import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
 
 import extractor.com.myapplication.R;
 
 public class EasyFinanceActivity extends ActionBarActivity {
-    private static String TAG = "LOG";
     private Toolbar mToolbar;
 
     @Override
@@ -27,13 +26,13 @@ public class EasyFinanceActivity extends ActionBarActivity {
 
         mToolbar = (Toolbar) findViewById(R.id.tb_main);
         mToolbar.setTitle("Easy Finance");
-        mToolbar.setLogo(R.mipmap.ic_launcher);
         setSupportActionBar(mToolbar);
 
         new Drawer()
             .withActivity(this)
             .withToolbar(mToolbar)
             .addDrawerItems(
+                    new ProfileDrawerItem().withName("Muryllo Tiraza"),
                     new PrimaryDrawerItem().withName("Home"),
                     new DividerDrawerItem(),
                     new SecondaryDrawerItem().withName("Configurações")
@@ -41,10 +40,11 @@ public class EasyFinanceActivity extends ActionBarActivity {
             .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
                 @Override
                 public void onItemClick(AdapterView<?> parent, View view, int position, long id, IDrawerItem drawerItem) {
-                    // do something with the clicked item :D
+                    Toast.makeText(getApplicationContext(), "Menu: " + position, Toast.LENGTH_SHORT).show();
                 }
             })
             .build();
     }
+
 
 }
